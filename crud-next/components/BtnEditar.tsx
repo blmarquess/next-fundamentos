d9cfa08a6f1aeec8ef7288f8d7a9380660913171
Bruno Marques <blmarques.dev@gmail.com>
2022-02-28 20:48:01 -0300

    feat(btnedituser): add component BTN Edit for inline table event fo edit usaer

---
+++ crud-next/components/BtnEditar.tsx
@@ -0,0 +1,13 @@
+export default function BtnEdit(): JSX.Element {
+  return (
+    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+      >
+      </path>
+    </svg>
+  );
+}
