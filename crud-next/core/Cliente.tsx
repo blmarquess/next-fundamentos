0a54c28f119261417d8c74c9f872c10063f297a6
Bruno Marques <blmarques.dev@gmail.com>
2022-02-28 03:18:27 -0300

    feat(classcliente): add class Cliente

---
+++ crud-next/core/Cliente.tsx
@@ -0,0 +1,27 @@
+import { threadId } from "worker_threads"
+
+export default class Cliente {
+  #id: string
+  #name: string
+  #age: number
+
+  constructor(name: string, age: number, id: string = `${Math.random()}`) {
+    this.#name = name
+    this.#age = age
+    this.#id = id
+  }
+
+  static vazio() {
+    return new Cliente('', 0)
+  }
+
+  get id() {
+    return this.#id;
+  }
+  get name() {
+    return this.#name;
+  }
+  get age() {
+    return this.#age;
+  }
+}
