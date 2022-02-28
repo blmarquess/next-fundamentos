d14227ed6af18ed6021d5528668f90cc36bc2b2f
Bruno Marques <blmarques.dev@gmail.com>
2022-02-28 02:13:07 -0300

    feat(navlinks): add style and links to navigation

--- crud-next/components/NavLink.tsx
+++ crud-next/components/NavLink.tsx
@@ -7,7 +7,8 @@ interface NavBarProps{
 
 export function NavLink({destination, title}:NavBarProps):JSX.Element {
   return(
-    <span className="font-bold mx-2 border-b-4 border-white">
+    <span className={ `font-bold px-3 mx-4 border-solid border-b-2 border-blue-900
+    hover:border-white cursor-pointer rounded-b-md` }>
       <Link href={ destination }>
       <a>
         {title}
