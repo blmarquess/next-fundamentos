c670b2dd25ec4c5f51c7d66d050b610c86e3d8e8
Bruno Marques <blmarques.dev@gmail.com>
2022-03-01 01:44:08 -0300

    feat(inputs): add types and events for manipulation of the states

--- crud-next/components/Input.tsx
+++ crud-next/components/Input.tsx
@@ -1,10 +1,32 @@
-interface InputProps { textlabel: string, value: string }
+interface InputProps {
+  textlabel: string;
+  type: string;
+  value: string | number;
+  disable?: boolean;
+  name: string;
+  update: Function;
+}
 
-export default function Input({textlabel, value }:InputProps) {
+export default function Input({
+  textlabel,
+  value,
+  disable,
+  name,
+  update,
+  type,
+}: InputProps) {
   return (
-    <label htmlFor="impt">
-      { textlabel }
-      <input type="text" id="impt" value={ value } />
-    </label>
+    <div className="flex w-full items-center justify-between py-1">
+      <label htmlFor="impt">{textlabel}</label>
+      <input
+        type={type}
+        id="impt"
+        onChange={({ target: { name, value } }) => update(name, value)}
+        name={name}
+        disabled={disable ?? false}
+        value={value}
+        className="outline-blue-600 py-2 px-3 mb-2 ml-2 border-solid border border-gray-500 rounded-md grow-1 w-full"
+      />
+    </div>
   );
 }
