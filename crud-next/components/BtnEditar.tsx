8ff923095249b233ab6d763910c5241263727e44
Bruno Marques <blmarques.dev@gmail.com>
2022-02-28 21:54:58 -0300

    style(btns edit/delete): style css and clean code

--- crud-next/components/BtnEditar.tsx
+++ crud-next/components/BtnEditar.tsx
@@ -1,13 +1,23 @@
 export default function BtnEdit(): JSX.Element {
   return (
-    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+    <button
+    type="button"
+    >
+      <svg
+        className="w-6 h-6 rounded-full p-1 bg-orange-100 hover:bg-orange-500"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
       >
-      </path>
-    </svg>
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+        >
+        </path>
+      </svg>
+    </button>
   );
 }
