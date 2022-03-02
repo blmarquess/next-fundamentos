2b572ebeca52dcdd416dd96f4c210b1c63d6e0a9
Bruno Marques <blmarques.dev@gmail.com>
2022-03-02 04:13:52 -0300

    feat(firebaseconfig): add new config firebase and env_vars
    including settings as per current Firebase documentation and environment variables

---
+++ crud-next/backend/config.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const dbase = getFirestore(app);
+
+export default dbase;
