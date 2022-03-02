6eb435229720823e586f2aab14b402a7e8301a49
Bruno Marques <blmarques.dev@gmail.com>
2022-03-02 04:09:35 -0300

    fix(conectiondbbycod3r): remove code of course cod3r. Not working atual version firebase
    Removendo codigo encinado no curso da coud3r, porque não funciona com a versão stual do firebase

--- crud-next/backend/config-cold3r.ts
+++ crud-next/backend/config-cold3r.ts
@@ -1,8 +1,9 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 import 'firebase/sirestore';
 
 if(!firebase.apps.langth) {
-  firebase.initializeApp({
+  initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
