d0183f03fa695d3bf57d1aa598cc46e254d20123
Bruno Marques <blmarques.dev@gmail.com>
2022-03-02 03:11:29 -0300

    feat(clouddatabase): add connection local to cloude firestore

--- crud-next/pages/index.tsx
+++ crud-next/pages/index.tsx
@@ -1,16 +1,28 @@
-import Layout from '../components/Layout'; 
+import { useEffect, useState } from "react";
+import CollectionClients from "../backend/database/CollectionClients";
+import Layout from "../components/Layout";
+import TableList from "../components/TableList";
+import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 const Home: React.FC = (): JSX.Element => {
-  return(
+  const [clientsListe, setClientsList] = useState<Cliente[]>([]);
+
+  useEffect(() => {
+    const repo: ClienteRepositorio = new CollectionClients();
+    repo.getAllCli().then((res) => {
+      setClientsList(res);
+      localStorage.setItem("fire", JSON.stringify(res));
+    });
+  }, []);
+
+  return (
     <Layout>
       <div>
-        <h1>App Title</h1>
-        <p>
-          Testando a aplicação
-        </p>
+        <TableList clients={clientsListe ?? []} />
       </div>
     </Layout>
   );
-}
+};
 
 export default Home;
