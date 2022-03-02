80d4e2c2180c96b29f19834d24af309ab8086a1a
Bruno Marques <blmarques.dev@gmail.com>
2022-03-02 16:41:38 -0300

    feat(formbuttons): add props for exec functions on click

--- crud-next/components/BtnEditar.tsx
+++ crud-next/components/BtnEditar.tsx
@@ -1,8 +1,10 @@
-export default function BtnEdit(): JSX.Element {
+interface BtnProps {
+  func: () => void;
+}
+
+export default function BtnEdit({ func }: BtnProps): JSX.Element {
   return (
-    <button
-    type="button"
-    >
+    <button type="button" onClick={func}>
       <svg
         className="w-6 h-6 rounded-full p-1 bg-orange-50 hover:bg-orange-300"
         fill="none"
@@ -15,8 +17,7 @@ export default function BtnEdit(): JSX.Element {
           strokeLinejoin="round"
           strokeWidth="2"
           d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-        >
-        </path>
+        ></path>
       </svg>
     </button>
   );
