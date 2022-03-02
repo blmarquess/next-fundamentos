ed0ea37ca58bbe2bf7115e771e27e890169565ed
Bruno Marques <blmarques.dev@gmail.com>
2022-03-02 16:47:55 -0300

    feat(userslist): add function of delete user of cloud db

--- crud-next/pages/index.tsx
+++ crud-next/pages/index.tsx
@@ -4,18 +4,25 @@ import Layout from "../components/Layout";
 import TableList from "../components/TableList";
 import Cliente from "../core/Cliente";
 import dbase from '../backend/config';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import { useRouter } from "next/router";
 // import ClienteRepositorio from "../core/ClienteRepositorio";
 
 const Home: React.FC = (): JSX.Element => {
   const [clientsListe, setClientsList] = useState<Cliente[]>([]);
-  const usersCollectionRef = collection(dbase, 'users');
+  const usersCollectionRef = collection(dbase, "users");
+  const router = useRouter();
 
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
+      console.log(data.docs?.map((dc) => ({ ...dc.data(), id: dc.id })));
       const users = data.docs
-        ?.map((doc) => ({ ...doc.data() }))
+        ?.map((doc) => ({
+          name: doc.data().name,
+          age: doc.data().age,
+          id: doc.id,
+        }))
         .map((us) => new Cliente(us.name, us.age, us.id));
 
       if (users.length > clientsListe.length) {
@@ -27,10 +34,16 @@ const Home: React.FC = (): JSX.Element => {
     getUsers();
   }, [clientsListe.length, usersCollectionRef]);
 
+  async function deletUser(id: string) {
+    const userDoc = doc(dbase, "users", id);
+    await deleteDoc(userDoc);
+    router.reload();
+  }
+
   return (
     <Layout>
       <div>
-        <TableList clients={clientsListe ?? []} />
+        <TableList clients={clientsListe ?? []} fcdelete={deletUser} />
       </div>
     </Layout>
   );
