c437367d7dd0103b58cbe5098f5ceb1c275a0195
Bruno Marques <blmarques.dev@gmail.com>
2022-03-02 04:07:02 -0300

    feat(firebase): listing all entris on firebase
    Leitura do banco na cloud do google bem sucedida

--- crud-next/pages/index.tsx
+++ crud-next/pages/index.tsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
-import CollectionClients from "../backend/database/CollectionClients";
+// import CollectionClients from "../backend/database/CollectionClients";
 import Layout from "../components/Layout";
 import TableList from "../components/TableList";
 import Cliente from "../core/Cliente";
-import ClienteRepositorio from "../core/ClienteRepositorio";
+import dbase from '../backend/config';
+import { collection, getDocs } from 'firebase/firestore';
+// import ClienteRepositorio from "../core/ClienteRepositorio";
 
 const Home: React.FC = (): JSX.Element => {
   const [clientsListe, setClientsList] = useState<Cliente[]>([]);
+  const usersCollectionRef = collection(dbase, 'users');
 
   useEffect(() => {
-    const repo: ClienteRepositorio = new CollectionClients();
-    repo.getAllCli().then((res) => {
-      setClientsList(res);
-      localStorage.setItem("fire", JSON.stringify(res));
-    });
+  const getUsers = async () => {
+    const data = await getDocs(usersCollectionRef)
+    const users = data.docs?.map((doc) => ({ ...doc.data() }));
+    setClientsList(()=> ([...users.map((us) => new Cliente(us.name, us.age, us.id))]));
+    console.log([...users.map((us) => ({name: us.name, idade: us.age, id:us.id }))]);
+  };
+  getUsers();
   }, []);
 
   return (
