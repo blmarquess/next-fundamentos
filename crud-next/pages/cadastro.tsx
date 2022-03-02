1efb37cbae834ae50ac88916b880315402dcbcc3
Bruno Marques <blmarques.dev@gmail.com>
2022-03-02 14:25:42 -0300

    feat(cadastro): add function to save data in cloud and send function to form

--- crud-next/pages/cadastro.tsx
+++ crud-next/pages/cadastro.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Form from "../components/Form";
+import { addDoc, collection } from "firebase/firestore";
+import dbase from "../backend/config";
+import Cliente from "../core/Cliente";
 
 export default function cadastro(): JSX.Element {
+  const usersCollectionRef = collection(dbase, "users");
+
+  const addUser = async (newUsr: Cliente) => {
+    await addDoc(usersCollectionRef, {
+      name: newUsr.name,
+      age: newUsr.age,
+      id: newUsr.id,
+    });
+  };
+
   return (
     <Layout>
-      <Form />
+      <Form funcForm={addUser} />
     </Layout>
   );
 }
