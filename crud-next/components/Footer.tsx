beb7d5e792652679facffcdaa7fd912fcf01a23a
Bruno Marques <blmarques.dev@gmail.com>
2022-03-03 17:24:21 -0300

    feat(footer): add description project on footer and remove /edit to NavMenu

--- crud-next/components/Footer.tsx
+++ crud-next/components/Footer.tsx
@@ -1,7 +1,12 @@
 export default function Footer() {
-  return(
-    <div className=" flex bg-gray-800 text-blue-200 h-20 w-full bottom-0">
-      Aqui é o footer
+  return (
+    <div className={`flex bg-gray-800 text-blue-200
+      min-h-20 w-full bottom-0 justify-center items-center p-4`}>
+      <div className="px-4 pb-0 pt-2 flex flex-wrap">
+        <p>Projeto desencolvido apenas para estudar as estruturas do Next.JS
+          e iniciar os estudos na parte de backend/cloud com Firestore
+        </p>
+      </div>
     </div>
-  )
+  );
 }
--- crud-next/components/NavBar.tsx
+++ crud-next/components/NavBar.tsx
@@ -4,8 +4,8 @@ import { NavLink } from "./NavLink";
 export default function NavBar() {
   return(
     <nav className="flex justify-center items-center bg-blue-900 text-white p-8 h-20 w-full">
-      <NavLink destination="/cadastro" title="Cadastrar" />
-      <NavLink destination="/edition" title="Editar" />
+      <NavLink destination="/cadastro" title="Cadastrar Usuario" />
+      {/* <NavLink destination="/edition" title="Editar" /> */}
       <NavLink destination="/listus" title="Listar Usuarios" />
     </nav>
   );
