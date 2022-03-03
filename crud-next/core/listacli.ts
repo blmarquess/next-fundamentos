39606a72bf686c05498e58af981a2a27e30c7d34
Bruno Marques <blmarques.dev@gmail.com>
2022-03-03 00:13:09 -0300

    fix(mock): Add id random for all user list of mock

--- crud-next/core/listacli.ts
+++ crud-next/core/listacli.ts
@@ -1,19 +1,19 @@
 import Cliente from "./Cliente";
 
 export const clientList = [
-  new Cliente('Jaqueline', 19),
-  new Cliente('Hellen', 26),
-  new Cliente('Vitoria', 24),
-  new Cliente('Amanda', 44),
-  new Cliente('Bia', 42),
-  new Cliente('Soraia', 34),
-  new Cliente('Manoela', 24),
-  new Cliente('Nubia', 28),
-  new Cliente('Ana', 18),
-  new Cliente('Fernanda', 38),
-  new Cliente('Daniela', 31),
-  new Cliente('Samanta', 25),
-  new Cliente('Geovana', 23),
+  new Cliente('Jaqueline', 19, Math.random().toString(16)),
+  new Cliente('Hellen', 26, Math.random().toString(16)),
+  new Cliente('Vitoria', 24, Math.random().toString(16)),
+  new Cliente('Amanda', 44, Math.random().toString(16)),
+  new Cliente('Bia', 42, Math.random().toString(16)),
+  new Cliente('Soraia', 34, Math.random().toString(16)),
+  new Cliente('Manoela', 24, Math.random().toString(16)),
+  new Cliente('Nubia', 28, Math.random().toString(16)),
+  new Cliente('Ana', 18, Math.random().toString(16)),
+  new Cliente('Fernanda', 38, Math.random().toString(16)),
+  new Cliente('Daniela', 31, Math.random().toString(16)),
+  new Cliente('Samanta', 25, Math.random().toString(16)),
+  new Cliente('Geovana', 23, Math.random().toString(16)),
 ];
 
 // https://www.youtube.com/watch?v=jCY6DH8F4oc
